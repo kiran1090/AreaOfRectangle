d8f149b0e98cf6a375a41362b6eaba042d55d985
kiran_a <iamkiranashok@gmail.com>
2023-01-18 12:26:07 +0530

    Second commit

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangle {
+    public Double length,breadth,Area;
+    public  Rectangle(Double length, Double breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+
+    }
+
+    public Double getArea() {
+        return Area=length*breadth;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", breadth=" + breadth +
+                ", Area=" + Area +
+                '}';
+    }
+
+    public static void main(String[] args)
+    {
+        Rectangle Area1= new Rectangle(4.0,5.0);
+        Rectangle Area2= new Rectangle(5.0,8.0);
+        Area1.getArea();
+        System.out.println(Area1);
+        System.out.println(Area2.getArea());
+
+
+
+
+    }
 }
